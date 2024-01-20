0d5bf64ca7760af9a77cc79208190e88b4f56322
Benson306 <bnkimtai@gmail.com>
2024-01-20 23:28:46 +0300

    Create Single Payout

--- src/pages/SimpleForm.js
+++ src/pages/SimpleForm.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Input, HelperText, Label, Select, Textarea } from '@windmill/react-ui'
+import PageTitle from '../components/Typography/PageTitle'
+import SectionTitle from '../components/Typography/SectionTitle'
 
 function SimpleForm() {
   return (
-    <div>Simple Form</div>
+    <div>
+      <PageTitle>Create Single Payout</PageTitle>
+    </div>
   )
 }
 
